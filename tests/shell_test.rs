91a2d9180b9ea3a8db676900388ab0e0ac88467b
Dmytro Yemelianov <dyd@sitecore.net>
2026-01-06 15:31:30 +0200

    feat: add interactive shell mode test

---
+++ tests/shell_test.rs
@@ -0,0 +1,23 @@
+use std::process::{Command, Stdio};
+use std::io::Write;
+
+#[test]
+fn test_interactive_shell_welcome_message() {
+    let mut cmd = Command::new("cargo");
+    let mut child = cmd.args(&["run", "--bin", "raps", "--", "shell"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    {
+        let stdin = child.stdin.as_mut().unwrap();
+        stdin.write_all(b"exit\n").unwrap();
+    }
+
+    let output = child.wait_with_output().unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
+
+    assert!(output.status.success());
+    assert!(stdout.contains("Welcome to the RAPS interactive shell!"));
+}
